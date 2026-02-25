d89f5f5b7fc07ec8c5d089310f96e629c66faba1
logsuportejwm-collab <logsuportejwm@gmail.com>
2026-02-25 15:52:57 -0300

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,10 @@ def get_connection():
         user=st.secrets["mysql"]["user"],
         password=st.secrets["mysql"]["password"],
         database=st.secrets["mysql"]["database"],
-        port=st.secrets["mysql"]["port"]
+        port=st.secrets["mysql"]["port"],
+
+        connection_timeout=5, 
+        autocommit=True
     )
 
 # =========================================================
